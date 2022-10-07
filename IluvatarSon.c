67655e1a2b92bf3d52f4613f81bddbb0b564b495
claudia.lajara <claudia.lajara@montserrat.salle.url.edu>
2022-10-07 18:38:11 +0200

    Initial commit

---
+++ IluvatarSon.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+// OTHER LIBS
+
+// CONSTANTS
+
+// TYPEDEFS
+
+// GLOBAL VARS
+
+// FUNCTIONS
+/*********************************************************************
+ @Purpose: ----
+ @Params: ----
+ @Return: ----
+*********************************************************************/
+
+/*********************************************************************
+* @Purpose: Executes the IluvatarSon process.
+* @Params: in: argc = number of arguments entered
+*          in: argv = array of arguments entered
+* @Return: Returns 0.
+*********************************************************************/
+int main(int argc, char* argv[]) {
+    //
+
+	return (0);
+}
